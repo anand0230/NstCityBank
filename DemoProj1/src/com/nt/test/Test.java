47bec927a09ce85a7cf033b02f75bf32ba30b84e
Dev2 <Dev2@gmail.com>
2024-04-29 02:13:45 +0530

    Some Checking

--- DemoProj1/src/com/nt/test/Test.java
+++ DemoProj1/src/com/nt/test/Test.java
@@ -1,5 +1,12 @@
 package com.nt.test;
 
+import com.nt.commons.Utility;
+
 public class Test {
+	
+public static void main(String[] args) {
+	Utility u=new Utility();
+	System.out.println(u.getSysDate());
+}
 
 }
