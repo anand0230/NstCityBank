85e007eccbfc295ecba7997ded66bb0445461f8b
Dev2 <Dev2@gmail.com>
2024-04-29 04:22:39 +0530

    Merge Both Branch main and otpService

--- DemoProj1/src/com/nt/commons/F1.java
+++ DemoProj1/src/com/nt/commons/F1.java
@@ -2,7 +2,7 @@ package com.nt.commons;
 
 public class F1 {
 	public void f1() {
+		
 		System.out.println("f1");
 	}
-
-}
+	}
--- DemoProj1/src/com/nt/commons/Utility.java
+++ DemoProj1/src/com/nt/commons/Utility.java
@@ -4,10 +4,13 @@ import java.util.Date;
 
 public class Utility {
 	
+	
 public Date getSysDate() {
 	Date det=new Date();
 	return det;
 }
+
+
 public void m1() {
 	System.out.println("Utility.m1()..1234");
 	System.out.println("Anand");
